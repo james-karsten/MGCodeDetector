49d101da4dedc2d721b515bedeae3408590c40d4
james <jameskarsten007@gmail.com>
2024-06-24 13:11:34 +0200

    Removed Main function frommM107

--- Google_tests/malicious-gcode-detector/temp/M107/M107.cpp
+++ Google_tests/malicious-gcode-detector/temp/M107/M107.cpp
@@ -52,7 +52,4 @@ TEST_F(GCodeSecurityDispatcherTest, M107InValidGcode106FansOff) {
 
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+
