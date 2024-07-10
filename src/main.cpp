9f811d640d0da0f2ada301e413a9b3955365c728
james <jameskarsten007@gmail.com>
2024-07-10 18:02:49 +0200

    Added experiments code

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,114 @@
 #include "parser.h"
 #include <string>
 #include "../src/gcode-file-reader/GCodeFileReader.h"
+#include <filesystem>
+#include <fstream>
+#include <list> // for list operations
+#include <vector>
+#include "malicious-gcode-detector/GCodeSecurityDispatcher.h"
+
+
+// declaring list
+std::vector<std::string> valid_gcodes;
+std::vector<std::string> invalid_gcodes;
+std::vector<std::string> malicious_gcodes;
+std::vector<std::string> benign_gcodes;
+
+
+
+void read_gcode_file_exp1(const std::string& filename) {
+
+    // Check .gco file extension
+    if (filename.size() < 6 || filename.substr(filename.size() - 6) != ".gcode") {
+        std::cerr << "File Error: " << filename << " is not a .gcode file" << std::endl;
+        return;
+    }
+
+    // Check if file can be opened
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cerr << "File Error: Could not open file " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    bool gcode_validation;
+    while (std::getline(infile, line)) {
+
+        /* skip line that starts with ; (comment*/
+        if (line[0] == ';' || line.empty()) {
+            continue;
+        }
+
+        gcode_validation = GCodeParser::detect_valid_gcode(const_cast<char *>(line.c_str()), true);
+
+        if (gcode_validation) {
+            valid_gcodes.push_back(line);
+        } else {
+            invalid_gcodes.push_back(line);
+        }
+
+    }
+
+    infile.close();
+}
+
+/**
+ * Experiment 1 for invalid/valid g-codes
+ * @return
+ */
+int experiment_valid_invalid_gcodes() {
+    std::string directory = "C:\\Users\\james\\ClionProjects\\untitled\\files\\gcode\\valid\\";
+
+    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+        if (entry.path().extension() == ".gcode") {
+            std::string filename = entry.path().string();
+            std::cout << "Analyzing file: " << filename << std::endl;
+            read_gcode_file_exp1(filename);
+        }
+        std::cout << "  "  << std::endl;
+    }
+
+    /* print items */
+    std::cout << "valid G-codes: " << std::endl;
+    for (auto& code : valid_gcodes) {
+        std::cout << code << std::endl;
+    }
+
+    std::cout << " =====================" << std::endl;
+    std::cout << "Invalid G-codes: " << std::endl;
+    for (auto& code : invalid_gcodes) {
+        std::cout << code << std::endl;
+    }
+
+
+    return 0;
+}
+
+
+
+/**
+ * Experiment 2 for benign/malicious g-codes
+ * @return
+ */
+int experiment_benign_malicious_gcodes() {
+    std::string directory = "C:\\Users\\james\\ClionProjects\\untitled\\files\\gcode\\benign\\";
+
+    for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+        if (entry.path().extension() == ".gcode") {
+            std::string filename = entry.path().string();
+            std::cout << "Analyzing file: " << filename << std::endl;
+            GCodeFileReader::read_gcode_file(filename);
+        }
+        std::cout << "  "  << std::endl;
+    }
+
+    std::cout << "Done" << std::endl;
+    return 0;
+}
+
 
 int main() {
-    // std::string filename = "C:\\Users\\james\\CLionProjects\\untitled\\files\\gcode\\Prusa_200um_MINI_PLA_23m.gcode";
-//    std::string filename = "C:\\Users\\james\\CLionProjects\\untitled\\files\\gcode\\tests\\M107\\M107_valid_gcode.gcode";
-//    GCodeFileReader::read_gcode_file(filename);
-//
-//    std::cout << "Done" << std::endl;
-
-    char testLine2[] = "M710 A1 S255 I128";
-    char testLine3[] = "M710 A0 S255";
-    char testLine4[] = "M710 R";
-    char testLine5[] = "M710 A0 S2550";
-    char testLine6[] = "M710 A1 S245 I1228";
-    char testLine7[] = "M710 A1 S2245 I28";
-    char testLine8[] = "M710 A1 S0 I20.8";
-    char testLine9[] = "M710 A1 S2245.8 I28";
-    char testLine10[] = "M710 A1 S255 I0";
-    char testLine11[] = "M303 S0 U";
-    char testLine12[] = "M303 S5210";
-    char testLine13[] = "M303 E3 C8 S21";
-    GCodeParser::parse(testLine2);
-    GCodeParser::parse(testLine3);
-    GCodeParser::parse(testLine4);
-    GCodeParser::parse(testLine5);
-    GCodeParser::parse(testLine6);
-    GCodeParser::parse(testLine7);
-    GCodeParser::parse(testLine8);
-    GCodeParser::parse(testLine9);
-    GCodeParser::parse(testLine10);
-    GCodeParser::parse(testLine11);
-    GCodeParser::parse(testLine12);
-    GCodeParser::parse(testLine13);
+    experiment_benign_malicious_gcodes();
 }
+
